b26ae0d2ed39691ef98acfc088803edd76906c5b
chinadarlone-glitch <ebaneth1402@gmail.com>
2026-02-16 11:35:18 +0100

    checking statut paiement v4

--- mk_2bn_market/backend/models/transaction.js
+++ mk_2bn_market/backend/models/transaction.js
@@ -33,7 +33,7 @@ const transactionSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum: ['pending', 'completed', 'failed', 'cancelled'],
+    enum: ['pending','processed', 'completed', 'failed', 'cancelled'],
     default: 'pending'
   },
   paid_at: Date
